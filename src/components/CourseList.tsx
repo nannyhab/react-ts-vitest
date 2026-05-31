78b76530fd96979996cb77666cf6adf1576d3725
Nanny Hab <adnanalhabian@gmail.com>
2026-05-31 16:57:41 -0500

    React-challenge implementing the selectable courses

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useState } from "react";
 
 type Course = {
   term: string;
@@ -15,18 +16,32 @@ type Course = {
 
 type Courses = Record<string, Course>;
 
+
 const CourseList = ({ courses }: { courses: Courses }) => {
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const toggle = (id: string) => {
+    if (selected.includes(id)) {
+      setSelected(selected.filter(x => x !== id));
+    } else {
+      setSelected([...selected, id]);
+    }
+  };
+
   return (
     
     <div className="flex flex-row flex-wrap gap-4 mt-6 w-full justify-center">
       {Object.entries(courses).map(([id, course]) => (
-              <Card key={id} className="w-64">
+        <Card key={id}
+        onClick={() => toggle(id)}
+        className={selected.includes(id) ? "w-64 bg-green-200" : "w-64"}>
         <CardHeader>
           <CardDescription>{course.term} · CS {course.number}</CardDescription>
           <CardTitle>{course.title}</CardTitle>
         </CardHeader>
         <CardContent>
           <p className="text-sm text-muted-foreground">{course.meets}</p>
+          {selected.includes(id) && <span>✓ Selected</span>}
         </CardContent>
       </Card>
       ))}
