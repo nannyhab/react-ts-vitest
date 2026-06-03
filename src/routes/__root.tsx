028cc1039cefc3bfe01c65ade832123014cb29d9
Nanny Hab <adnanalhabian@gmail.com>
2026-06-03 09:00:54 -0500

    added the auth to the tan stack root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import Banner from "../components/Banner";
 
 export const Route = createRootRoute({
   component: () => (
     <div className="text-center">
-      <header className="bg-[#282c34] min-h-screen flex flex-col items-center text-white p-6">
-        <h1 className="text-2xl font-semibold mb-4">CS Course Scheduler</h1>
+      <Banner title="CS Course Scheduler" />
+      <main className="bg-[#282c34] min-h-screen text-white">
         <Outlet />
-      </header>
+      </main>
     </div>
   ),
 });
